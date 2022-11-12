cb9d070b030f0c8968ea38a883484d73c345f7ae
RainbowMango <qdurenhongcai@gmail.com>
2022-11-12 19:46:45 +0800

    comment v2.0.1

--- concurrent_map.go
+++ concurrent_map.go
@@ -29,38 +29,38 @@ type ConcurrentMapShared[K comparable, V any] struct {
 func create[K comparable, V any](sharding func(key K) uint32) ConcurrentMap[K, V] {
 	m := ConcurrentMap[K, V]{
 		sharding: sharding,
-		shards:   make([]*ConcurrentMapShared[K, V], SHARD_COUNT),
+		shards:   make([]*ConcurrentMapShared[K, V], SHARD_COUNT), // 预留32个槽位
 	}
 	for i := 0; i < SHARD_COUNT; i++ {
-		m.shards[i] = &ConcurrentMapShared[K, V]{items: make(map[K]V)}
+		m.shards[i] = &ConcurrentMapShared[K, V]{items: make(map[K]V)} // 32个槽位初始化
 	}
 	return m
 }
 
 // Creates a new concurrent map.
-func New[V any]() ConcurrentMap[string, V] {
+func New[V any]() ConcurrentMap[string, V] { // 创建方式一：创建string类型的map
 	return create[string, V](fnv32)
 }
 
 // Creates a new concurrent map.
-func NewStringer[K Stringer, V any]() ConcurrentMap[K, V] {
+func NewStringer[K Stringer, V any]() ConcurrentMap[K, V] { // 创建方式二：创建任意类型的map
 	return create[K, V](strfnv32[K])
 }
 
 // Creates a new concurrent map.
-func NewWithCustomShardingFunction[K comparable, V any](sharding func(key K) uint32) ConcurrentMap[K, V] {
+func NewWithCustomShardingFunction[K comparable, V any](sharding func(key K) uint32) ConcurrentMap[K, V] { // 创建方式三：支持指定hash函数
 	return create[K, V](sharding)
 }
 
 // GetShard returns shard under given key
 func (m ConcurrentMap[K, V]) GetShard(key K) *ConcurrentMapShared[K, V] {
-	return m.shards[uint(m.sharding(key))%uint(SHARD_COUNT)]
+	return m.shards[uint(m.sharding(key))%uint(SHARD_COUNT)] // 根据hash值对SHARD_COUNT取模获得该key对应的槽位
 }
 
-func (m ConcurrentMap[K, V]) MSet(data map[K]V) {
+func (m ConcurrentMap[K, V]) MSet(data map[K]V) { // 类似于merge另一个map，如果元素数量多时，会多次加锁，可以分组
 	for key, value := range data {
 		shard := m.GetShard(key)
-		shard.Lock()
+		shard.Lock() // 因为要写，所以必须加写锁
 		shard.items[key] = value
 		shard.Unlock()
 	}
@@ -86,14 +86,14 @@ func (m ConcurrentMap[K, V]) Upsert(key K, value V, cb UpsertCb[V]) (res V) {
 	shard := m.GetShard(key)
 	shard.Lock()
 	v, ok := shard.items[key]
-	res = cb(ok, v, value)
+	res = cb(ok, v, value) // 如果value已存在，可以提供定制能力，比如将已存在value与当前value合并等，进而形行新的value.
 	shard.items[key] = res
 	shard.Unlock()
 	return res
 }
 
 // Sets the given value under the specified key if no value was associated with it.
-func (m ConcurrentMap[K, V]) SetIfAbsent(key K, value V) bool {
+func (m ConcurrentMap[K, V]) SetIfAbsent(key K, value V) bool { // 只有key值不存在时才写入，可用于避免覆盖的场景。
 	// Get map shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -117,7 +117,7 @@ func (m ConcurrentMap[K, V]) Get(key K) (V, bool) {
 }
 
 // Count returns the number of elements within the map.
-func (m ConcurrentMap[K, V]) Count() int {
+func (m ConcurrentMap[K, V]) Count() int { // 统计所有槽位的元素数量
 	count := 0
 	for i := 0; i < SHARD_COUNT; i++ {
 		shard := m.shards[i]
@@ -129,7 +129,7 @@ func (m ConcurrentMap[K, V]) Count() int {
 }
 
 // Looks up an item under specified key
-func (m ConcurrentMap[K, V]) Has(key K) bool {
+func (m ConcurrentMap[K, V]) Has(key K) bool { // 判断元素是否存在
 	// Get shard
 	shard := m.GetShard(key)
 	shard.RLock()
@@ -140,7 +140,7 @@ func (m ConcurrentMap[K, V]) Has(key K) bool {
 }
 
 // Remove removes an element from the map.
-func (m ConcurrentMap[K, V]) Remove(key K) {
+func (m ConcurrentMap[K, V]) Remove(key K) { // 删除一个元素
 	// Try to get shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -155,7 +155,7 @@ type RemoveCb[K any, V any] func(key K, v V, exists bool) bool
 // RemoveCb locks the shard containing the key, retrieves its current value and calls the callback with those params
 // If callback returns true and element exists, it will remove it from the map
 // Returns the value returned by the callback (even if element was not present in the map)
-func (m ConcurrentMap[K, V]) RemoveCb(key K, cb RemoveCb[K, V]) bool {
+func (m ConcurrentMap[K, V]) RemoveCb(key K, cb RemoveCb[K, V]) bool { // 提供删除前确认的定制能力
 	// Try to get shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -169,7 +169,7 @@ func (m ConcurrentMap[K, V]) RemoveCb(key K, cb RemoveCb[K, V]) bool {
 }
 
 // Pop removes an element from the map and returns it
-func (m ConcurrentMap[K, V]) Pop(key K) (v V, exists bool) {
+func (m ConcurrentMap[K, V]) Pop(key K) (v V, exists bool) { // 弹出一个元素
 	// Try to get shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -180,7 +180,7 @@ func (m ConcurrentMap[K, V]) Pop(key K) (v V, exists bool) {
 }
 
 // IsEmpty checks if map is empty.
-func (m ConcurrentMap[K, V]) IsEmpty() bool {
+func (m ConcurrentMap[K, V]) IsEmpty() bool { // 判空
 	return m.Count() == 0
 }
 
@@ -193,19 +193,19 @@ type Tuple[K comparable, V any] struct {
 // Iter returns an iterator which could be used in a for range loop.
 //
 // Deprecated: using IterBuffered() will get a better performence
-func (m ConcurrentMap[K, V]) Iter() <-chan Tuple[K, V] {
+func (m ConcurrentMap[K, V]) Iter() <-chan Tuple[K, V] { // 通过管道获取所有值
 	chans := snapshot(m)
-	ch := make(chan Tuple[K, V])
+	ch := make(chan Tuple[K, V]) // 没能判断需要多少容量的管道，所以协程经常会被挂起
 	go fanIn(chans, ch)
 	return ch
 }
 
 // IterBuffered returns a buffered iterator which could be used in a for range loop.
-func (m ConcurrentMap[K, V]) IterBuffered() <-chan Tuple[K, V] {
+func (m ConcurrentMap[K, V]) IterBuffered() <-chan Tuple[K, V] { // 通过管道获取所有值(高性能版)
 	chans := snapshot(m)
 	total := 0
 	for _, c := range chans {
-		total += cap(c)
+		total += cap(c) // 判断需要预置的管道容量，所以协程不会被挂起
 	}
 	ch := make(chan Tuple[K, V], total)
 	go fanIn(chans, ch)
@@ -213,7 +213,7 @@ func (m ConcurrentMap[K, V]) IterBuffered() <-chan Tuple[K, V] {
 }
 
 // Clear removes all items from map.
-func (m ConcurrentMap[K, V]) Clear() {
+func (m ConcurrentMap[K, V]) Clear() { // 清理map
 	for item := range m.IterBuffered() {
 		m.Remove(item.Key)
 	}
@@ -236,13 +236,13 @@ func snapshot[K comparable, V any](m ConcurrentMap[K, V]) (chans []chan Tuple[K,
 		go func(index int, shard *ConcurrentMapShared[K, V]) {
 			// Foreach key, value pair.
 			shard.RLock()
-			chans[index] = make(chan Tuple[K, V], len(shard.items))
+			chans[index] = make(chan Tuple[K, V], len(shard.items)) // 超大缓冲区的channel
 			wg.Done()
 			for key, val := range shard.items {
 				chans[index] <- Tuple[K, V]{key, val}
 			}
 			shard.RUnlock()
-			close(chans[index])
+			close(chans[index]) // 提前关闭，不妨碍读取
 		}(index, shard)
 	}
 	wg.Wait()
@@ -266,7 +266,7 @@ func fanIn[K comparable, V any](chans []chan Tuple[K, V], out chan Tuple[K, V]) 
 }
 
 // Items returns all items as map[string]V
-func (m ConcurrentMap[K, V]) Items() map[K]V {
+func (m ConcurrentMap[K, V]) Items() map[K]V { // 全并成一个map返回，用于对接其他接口
 	tmp := make(map[K]V)
 
 	// Insert items to temporary map.
@@ -285,7 +285,7 @@ type IterCb[K comparable, V any] func(key K, v V)
 
 // Callback based iterator, cheapest way to read
 // all elements in a map.
-func (m ConcurrentMap[K, V]) IterCb(fn IterCb[K, V]) {
+func (m ConcurrentMap[K, V]) IterCb(fn IterCb[K, V]) { // 遍历map
 	for idx := range m.shards {
 		shard := (m.shards)[idx]
 		shard.RLock()
@@ -297,7 +297,7 @@ func (m ConcurrentMap[K, V]) IterCb(fn IterCb[K, V]) {
 }
 
 // Keys returns all keys as []string
-func (m ConcurrentMap[K, V]) Keys() []K {
+func (m ConcurrentMap[K, V]) Keys() []K { // 返回所有键值
 	count := m.Count()
 	ch := make(chan K, count)
 	go func() {
@@ -328,7 +328,7 @@ func (m ConcurrentMap[K, V]) Keys() []K {
 }
 
 // Reviles ConcurrentMap "private" variables to json marshal.
-func (m ConcurrentMap[K, V]) MarshalJSON() ([]byte, error) {
+func (m ConcurrentMap[K, V]) MarshalJSON() ([]byte, error) { // 转成json，可以由一行实现
 	// Create a temporary map, which will hold all item spread across shards.
 	tmp := make(map[K]V)
 
@@ -342,7 +342,7 @@ func strfnv32[K fmt.Stringer](key K) uint32 {
 	return fnv32(key.String())
 }
 
-func fnv32(key string) uint32 {
+func fnv32(key string) uint32 { // hash函数
 	hash := uint32(2166136261)
 	const prime32 = uint32(16777619)
 	keyLength := len(key)
@@ -354,7 +354,7 @@ func fnv32(key string) uint32 {
 }
 
 // Reverse process of Marshal.
-func (m *ConcurrentMap[K, V]) UnmarshalJSON(b []byte) (err error) {
+func (m *ConcurrentMap[K, V]) UnmarshalJSON(b []byte) (err error) { // 可以由MSet实现
 	tmp := make(map[K]V)
 
 	// Unmarshal into a single map.
